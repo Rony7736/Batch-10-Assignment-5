e5bfd7d4a97d96115dc75e04ded5676227af2071
Rony7736 <ronychakrabarty99@gmail.com>
2024-09-23 14:36:08 +0600

    add validation in input field

--- js/index.js
+++ js/index.js
@@ -13,6 +13,13 @@ document.getElementById('btn-donate-1').addEventListener('click', function(event
     const fixedAmountNumber = Number(fixedAmount)
     const totalDonateAmount = fixedAmountNumber - totalDonate
 
+    // Check Validation
+    if(input1 <= 0 || isNaN(input1)){
+        alert('Invalid Donation Amount')
+        return;
+    }
+
+
     document.getElementById('fixed-amount-1').innerText = totalDonate
 
     document.getElementById('fixed-amount').innerText = totalDonateAmount 
@@ -27,6 +34,14 @@ document.getElementById('btn-donate-2').addEventListener('click', function(event
     const fixedAmount = document.getElementById('fixed-amount').innerText
     const fixedAmountNumber = Number(fixedAmount)
     const totalDonateAmount = fixedAmountNumber - totalDonate
+
+    // Check Validation
+
+    if(input2 <= 0 || isNaN(input2)){
+        alert('Invalid Donation Amount')
+        return;
+    }
+
 
     document.getElementById('fixed-amount-2').innerText = totalDonate
 
@@ -44,6 +59,13 @@ document.getElementById('btn-donate-3').addEventListener('click', function(event
     const fixedAmount = document.getElementById('fixed-amount').innerText
     const fixedAmountNumber = Number(fixedAmount)
     const totalDonateAmount = fixedAmountNumber - totalDonate
+
+    // Check Validation
+    if(input3 <= 0 || isNaN(input3)){
+        alert('Invalid Donation Amount')
+        return;
+    }
+
 
     document.getElementById('fixed-amount-3').innerText = totalDonate
 
